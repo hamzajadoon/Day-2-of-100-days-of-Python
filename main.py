d06a80ade9e6c8a335e188c05cf6cd35315164f6
hamzajadoon <46929195+hamzajadoon@users.noreply.github.com>
2022-04-09 02:16:56 +0000

    added bmi simple calculator

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ two_digit_number = input("Type a two digit number: ")
 
 new_number = int(two_digit_number[0]) + int(two_digit_number[1])
 print(new_number)
+
+
+#Here I have created a simple bmi calculator in my day 2 python coding 
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi = int(weight) / float(height) ** 2
+print(int(bmi))
